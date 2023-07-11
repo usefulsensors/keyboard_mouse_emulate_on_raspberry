5bf94d4a99a9811a04a18e8db479ed62bafc3d81
Pete Warden <pete@usefulsensors.com>
2023-07-11 00:04:36 +0000

    Added input agent to boot script

--- boot.sh
+++ boot.sh
@@ -10,16 +10,6 @@ sudo /etc/init.d/bluetooth start
 #Get current Path
 export C_PATH=$(pwd)
 
+python3 server/input_agent.py 2>&1 >/tmp/input_agent_log.txt &
+
 sudo server/btk_server.py
-
-# tmux kill-window -t thanhle:app >/dev/null 2>&1
-
-# [ ! -z "$(tmux has-session -t thanhle 2>&1)" ] && tmux new-session -s thanhle -n app -d
-# [ ! -z "$(tmux has-session -t thanhle:app 2>&1)" ] && {
-#     tmux new-window -t thanhle -n app
-# }
-# [ ! -z "$(tmux has-session -t thanhle:app.1 2>&1)" ] && tmux split-window -t thanhle:app -h
-# [ ! -z "$(tmux has-session -t thanhle:app.2 2>&1)" ] && tmux split-window -t thanhle:app.1 -v
-# tmux send-keys -t thanhle:app.0 'cd $C_PATH/server && sudo ./btk_server.py' C-m
-# tmux send-keys -t thanhle:app.1 'cd $C_PATH/mouse  && reset' C-m
-# tmux send-keys -t thanhle:app.2 'cd $C_PATH/keyboard  && reset' C-m
